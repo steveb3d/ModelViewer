95051ed502ee2afbaf6ed50cd373a56ea1ad7161
steveb3d <63218140+steveb3d@users.noreply.github.com>
2020-04-13 18:42:11 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,160 @@
+// these need to be accessed inside more than one function so we'll declare them first
+let container;
+let camera;
+let controls;
+let renderer;
+let scene;
+
+const mixers = [];
+const clock = new THREE.Clock();
+
+function init() {
+
+  container = document.querySelector( '#scene-container' );
+
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color( 0x8FBCD7 );
+
+  createCamera();
+  createControls();
+  createLights();
+  loadModels();
+  createRenderer();
+
+  renderer.setAnimationLoop( () => {
+
+    update();
+    render();
+
+  } );
+
+}
+
+function createCamera() {
+
+  camera = new THREE.PerspectiveCamera( 45, container.clientWidth / container.clientHeight, 1, 1000 );
+  camera.position.set( 0, 10, -60 );
+  
+  
+ 
+}
+
+function createControls() {
+
+  controls = new THREE.OrbitControls( camera, container );
+  controls.screenSpacePanning = true;
+  ;
+}
+
+function createLights() {
+
+  const ambientLight = new THREE.HemisphereLight( 0xddeeff, 0x0f0e0d, 5 );
+  ambientLight.castShadow = true;
+  
+  const mainLight = new THREE.DirectionalLight( 0xffffff, 5 );
+  mainLight.position.set( 5, 10, 0 );
+  mainLight.castShadow = true;
+
+  
+  const secondLight = new THREE.DirectionalLight( 0xffffff, 5 );
+  secondLight.position.set( -5, 10, -30 );
+  secondLight.castShadow = true;
+
+  const thirdLight = new THREE.DirectionalLight( 0xffffff, 5 );
+  thirdLight.position.set( 0, -10, 30 );
+  thirdLight.castShadow = true;
+
+  scene.add( ambientLight, mainLight, secondLight, thirdLight );
+
+}
+
+function loadModels() {
+
+  const loader = new THREE.GLTFLoader();
+
+  // A reusable function to set up the models. We're passing in a position parameter
+  // so that they can be individually placed around the scene
+  const onLoad = ( gltf, position ) => {
+
+    const model = gltf.scene.children[ 0 ];
+    model.position.copy( position );
+
+    const animation = gltf.animations[ 0 ];
+
+    const mixer = new THREE.AnimationMixer( model );
+    mixers.push( mixer );
+
+    const action = mixer.clipAction( animation );
+    action.play();
+
+    scene.add( model );
+
+  };
+
+  // the loader will report the loading progress to this function
+  const onProgress = () => {};
+
+  // the loader will send any error messages to this function, and we'll log
+  // them to to console
+  const onError = ( errorMessage ) => { console.log( errorMessage ); };
+
+  // load the first model. Each model is loaded asynchronously,
+  // so don't make any assumption about which one will finish loading first
+  
+  const screwsPosition = new THREE.Vector3( 0, 0, 0 );
+  loader.load( 'models/Pump.glb', gltf => onLoad( gltf, screwsPosition ), onProgress, onError );
+
+
+
+}
+
+function createRenderer() {
+
+  // create a WebGLRenderer and set its width and height
+  renderer = new THREE.WebGLRenderer( { antialias: true } );
+  renderer.setSize( container.clientWidth, container.clientHeight );
+
+  renderer.setPixelRatio( window.devicePixelRatio );
+
+  renderer.gammaFactor = 2.2;
+  renderer.gammaInput = true;
+  renderer.gammaOutput = true;
+
+  renderer.physicallyCorrectLights = true;
+
+  container.appendChild( renderer.domElement );
+
+}
+
+function update() {
+
+  const delta = clock.getDelta();
+
+  for ( const mixer of mixers ) {
+
+    mixer.update( delta );
+
+  }
+
+}
+
+function render() {
+
+  renderer.render( scene, camera );
+
+}
+
+function onWindowResize() {
+
+  camera.aspect = container.clientWidth / container.clientHeight;
+
+  // update the camera's frustum
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( container.clientWidth, container.clientHeight );
+
+}
+
+window.addEventListener( 'resize', onWindowResize );
+
+init();
